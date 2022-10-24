635867f0fa8fa4e09bfd0a65f01765259e27cbf6
afsheen-01 <afsheen@amroliwala@gmail.com>
2022-10-24 16:58:33 +0530

    modifying tests

--- tests/deployments.spec.ts
+++ tests/deployments.spec.ts
@@ -9,20 +9,3 @@ beforeAll(() => {
 test("dummy", async () => {
   expect(true).toBeTruthy();
 });
-
-test("calling get deployment endoint (with params)", async () => {
-  const { response, error } = await listDeployments();
-  if (error) console.log(error);
-  expect(error).toBe(null);
-  expect(response?.status).toBe(200);
-});
-
-//we'll have to mock something like this
-// test.only("get deployment files(without params)", async () => {
-//   const { response, error } = await listDeploymentFiles({
-//     deploymentId: "6a03f61",
-//   });
-//   if (error) console.log(error);
-//   expect(error).toBe(null);
-//   expect(response?.status).toBe(200);
-// });
